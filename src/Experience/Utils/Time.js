52a86a1d6a3f7a188e96c2c8bed74bbda05d2847
leopantsulaia <leopantsulaia@gmail.com>
2025-05-16 19:19:16 +0400

    let's see now

--- src/Experience/Utils/Time.js
+++ src/Experience/Utils/Time.js
@@ -1,63 +1,50 @@
-import EventEmitter from './EventEmitter.js'
+import EventEmitter from './EventEmitter.js';
 
-export default class Time extends EventEmitter
-{
-    /**
-     * Constructor
-     */
-    constructor()
-    {
-        super()
+export default class Time extends EventEmitter {
+    constructor() {
+        super();
 
-        this.start = Date.now()
-        this.current = this.start
-        this.elapsed = 0
-        this.delta = 16
-        this.playing = true
+        this.start = performance.now();
+        this.current = this.start;
+        this.elapsed = 0;
+        this.delta = 16; // Approx. 60 FPS
+        this.playing = true;
+        this.lastTimestamp = 0;
 
-        this.tick = this.tick.bind(this)
-        this.tick()
+        this.tick = this.tick.bind(this);
+        this.ticker = requestAnimationFrame(this.tick);
     }
 
-    play()
-    {
-        this.playing = true
+    play() {
+        this.playing = true;
     }
 
-    pause()
-    {
-        this.playing = false
+    pause() {
+        this.playing = false;
     }
 
-    /**
-     * Tick
-     */
-    tick()
-    {
-        this.ticker = window.requestAnimationFrame(this.tick)
+    tick(timestamp) {
+        // Limit frame rate to avoid lag spikes
+        const frameInterval = 1000 / 60; // 60 FPS
+        if (timestamp - this.lastTimestamp < frameInterval) {
+            this.ticker = requestAnimationFrame(this.tick);
+            return;
+        }
+        this.lastTimestamp = timestamp;
 
-        const current = Date.now()
+        const current = performance.now();
+        this.delta = Math.min(current - this.current, 60); // Prevent large delta jumps
+        this.elapsed += this.playing ? this.delta : 0;
+        this.current = current;
 
-        this.delta = current - this.current
-        this.elapsed += this.playing ? this.delta : 0
-        this.current = current
-
-        if(this.delta > 60)
-        {
-            this.delta = 60
+        if (this.playing) {
+            this.trigger('tick');
         }
 
-        if(this.playing)
-        {
-            this.trigger('tick')
-        }
+        this.ticker = requestAnimationFrame(this.tick);
     }
 
-    /**
-     * Stop
-     */
-    stop()
-    {
-        window.cancelAnimationFrame(this.ticker)
+    stop() {
+        cancelAnimationFrame(this.ticker);
     }
 }
